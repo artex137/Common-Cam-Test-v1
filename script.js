8729bd0f1553e56ac013778404c26c76350ec7b8
artex137 <truthmonger01@gmail.com>
2023-08-29 00:11:53 -0400

    Your meaningful commit message here

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
+    function showLoadingOverlay() {
         const overlay = document.querySelector(".loading-overlay");
-        overlay.style.opacity = "0";
+        overlay.style.display = ""; // Make sure it's visible
+        overlay.style.opacity = "1";
+        
         setTimeout(function() {
-            overlay.style.display = "none";
-        }, 500);  // matches the CSS transition duration
-    }, 7000);  // 7 seconds
+            overlay.style.opacity = "0";
+            setTimeout(function() {
+                overlay.style.display = "none";
+            }, 500);  // matches the CSS transition duration
+        }, 7000);  // 7 seconds
+    }
+
+    showLoadingOverlay();  // Initial load
+
+    document.addEventListener("visibilitychange", function() {
+        if (document.visibilityState === "visible") {
+            showLoadingOverlay();
+        }
+    });
 });
